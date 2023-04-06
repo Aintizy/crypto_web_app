15a3cd7d38e4e74a4e8c95fe620874468e8d6cd0
Ifeanyi <oisrael2000@gmail.com>
2023-04-06 13:09:57 +0100

    updated some styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
+import "./assets/sass/main.scss";
+
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <h1>Hello world</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+      perferendis corrupti odit, provident maxime laborum dolores. Voluptatem
+      officia a tenetur qui blanditiis, minus porro eaque culpa voluptates nisi
+      quos recusandae!
+    </p>
   </React.StrictMode>
 );
 
